9657a2c3d333e5321f0a15773fb0729f2ecb7015
LeonardoBlanca <lssilvatati@gmail.com>
2024-04-26 20:55:57 -0300

    new: Created all the files to practice events
    - Positive numbers deposit, negative numbers withdraw.

---
+++ EventsSolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace EventsSolution
+{
+    public delegate void BalanceEventHandler(decimal theValue);
+
+    class PiggyBank
+    {
+        private decimal m_bankBalance;
+        public event BalanceEventHandler BalanceChanged;
+
+        public decimal theBalance
+        {
+            set
+            { m_bankBalance = value; BalanceChanged(value); }
+            get { return m_bankBalance; }
+        }
+    }
+
+    class BalanceLogger
+    {
+        public void BalanceLog(decimal amount)
+        {
+            Console.WriteLine($"The balance amount is {amount}");
+        }
+    }
+
+    class BalanceWatcher
+    {
+        public void BalanceWatch(decimal amount)
+        {
+            if (amount > 500.0m)
+                Console.WriteLine($"You reached your saving goal! You have U$ {amount}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EventsSolution;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PiggyBank pb = new PiggyBank();
+        BalanceLogger bl = new BalanceLogger();
+        BalanceWatcher bw = new BalanceWatcher();
+
+        pb.BalanceChanged += bl.BalanceLog;
+        pb.BalanceChanged += bw.BalanceWatch;
+
+        string theStr;
+        do
+        {
+            Console.WriteLine("How much would you like to deposit?");
+            theStr = Console.ReadLine();
+            if(!theStr.Equals("exit"))
+            {
+                // Converting to decimal
+                decimal newVal = decimal.Parse(theStr);
+
+                pb.theBalance += newVal;
+            }
+            
+        } while (!theStr.Equals("exit"));
+    }
+}
